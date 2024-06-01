a04efed9a3a58cada0d452a728fc7f42719c9b3c
James Villegas <evil-tim@users.noreply.github.com>
2024-06-01 14:02:33 +0800

    Add additional sync items; Add local time

--- config/rclone/rclone.sh
+++ config/rclone/rclone.sh
@@ -2,5 +2,7 @@
 
 # rclone -v sync cal-gdrive:Documents '/data/Google Drive/Documents'
 # rclone -v sync cal-gdrive:Dump '/data/Google Drive/Dump'
-rclone -v sync /data cal-s3:local-files-gda530zq
+rclone -v sync /data/Documents       cal-s3:local-files-gda530zq/Documents
+rclone -v sync /data/Music           cal-s3-enc:Music
+rclone -v sync /data/HomeAssistant   cal-s3-enc:HomeAssistant
 
